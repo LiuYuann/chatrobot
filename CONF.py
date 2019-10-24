e41ee87c560e78a2f5bc65b6b81327ffc34b63c8
Marcus <Marcus.liu@eisoo.com>
2019-10-24 09:27:21 +0800

    Marcus:edit readme

--- CONF.py
+++ CONF.py
@@ -2,6 +2,6 @@
 目前机器人回复消息是调用第三方的API：https://www.ownthink.com/docs/bot/，
 你可查看相关文档并注册申请Appid
 """
-APPID = '0a6ff85d11e5f0353a506dd02f3c4a68'  # 你的appid
-USERID = 'ycLoVnsr'  # 你的userid
+APPID = ''  # 你的appid
+USERID = ''  # 你的userid
 PORT = 80  # 项目运行的端口
